1c987c22b110ccabdf9f1179ee9de90be3b378bf
Edwin <yatman.c@gmail.com>
2020-05-04 00:02:52 +0800

    Completed Chapter2

---
+++ app/taco-cloud-c2/src/main/java/com/sia/tacocloud/Taco.java
@@ -0,0 +1,17 @@
+package com.sia.tacocloud;
+import java.util.List;
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Data
+public class Taco {
+
+  @NotNull
+  @Size(min=5, message = "Name must be at least 5 characters long")
+  private String name;
+  @NotNull(message = "You must choose at least 1 ingredient")
+  private List<String> ingredients;
+
+}
