731553e3d6701532365fb11d6d110514a5cd0474
Edwin <yatman.c@gmail.com>
2020-05-03 18:05:52 +0800

    completed chapter1

--- app/taco-cloud/src/main/java/com/sia/tacocloud/TacoCloudApplication.java
+++ app/taco-cloud/src/main/java/com/sia/tacocloud/TacoCloudApplication.java
@@ -2,6 +2,8 @@ package com.sia.tacocloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class TacoCloudApplication {
@@ -10,4 +12,12 @@ public class TacoCloudApplication {
 		SpringApplication.run(TacoCloudApplication.class, args);
 	}
 
+
+	@Controller
+	public class HomeController{
+		@GetMapping("/")
+		public String home(){
+			return "home";
+		}
+	}
 }
